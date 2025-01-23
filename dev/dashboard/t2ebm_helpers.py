c480eea75cd945b4d95eb6ddfe7b1367db41efb5
Julius <julius.wilps@web.de>
2025-01-23 14:23:01 +0100

    Created dataset description

--- dev/dashboard/t2ebm_helpers.py
+++ dev/dashboard/t2ebm_helpers.py
@@ -80,3 +80,174 @@ def extract_graph(
     assert len(x_vals) == len(scores), "The number of bins and scores does not match."
 
     return EBMGraph(feature_name, feature_type, x_vals, scores, stds)
+
+def graph_to_text(
+    graph: EBMGraph,
+    include_description=True,
+    feature_format=None,
+    x_axis_precision=None,
+    y_axis_precision="auto",
+    confidence_bounds=True,
+    confidence_level=0.95,
+    max_tokens=3000,
+):
+    """Convert an EBMGraph to text. This is the text that we then pass to the LLM.
+
+    The function takes care of a variety of different formatting issues that can arise in the process of converting a graph to text.
+
+    Args:
+        graph (EBMGraph): The graph.
+        include_description (bool, optional): Whether to include a short descriptive preamble that describes the graph to the LLM. Defaults to True.
+        feature_format (_type_, optional): The format of the feature (continuous, cateorical, boolean). Defaults to None (auto-detect).
+        x_axis_precision (_type_, optional): The precision of the values on the x-axis. Defaults to None.
+        y_axis_precision (str, optional): The precision of the values on the x-axis. Defaults to "auto".
+        confidence_bounds (bool, optional): Whether to inlcude confidence bounds. Defaults to True.
+        confidence_level (float, optional): The desired confidence level of the bounds. Defaults to 0.95.
+        max_tokens (int, optional): The maximum number of tokens that the textual description of the graph can have. The function simplifies the graph so to fit into this token limit. Defaults to 3000.
+
+    Raises:
+        Exception: If an error occurs.
+
+    Returns:
+        str: The textual representation of the graph.
+    """
+    # a simple auto-detect for boolean feautres
+    try:
+        if (
+            len(graph.x_vals) == 2
+            and graph.x_vals[0].upper() == "FALSE"
+            and graph.x_vals[1].upper() == "TRUE"
+        ):
+            feature_format = "boolean"
+    except:
+        pass
+
+    # determine the feature format
+    if feature_format is None:
+        feature_format = graph.feature_type
+    if feature_format == "nominal":
+        feature_format = "categorical"
+
+    # the description of the graph depends on the feature format
+    if feature_format == "continuous":
+        description_text = (
+            "This graph represents a continuous-valued feature. The keys are intervals"
+            " that represent ranges where the function predicts the same value.\n\n"
+        )
+    elif feature_format == "categorical":
+        description_text = (
+            "This graph represents categorical feature. Each key represents a possible"
+            " value that the feature can take.\n\n"
+        )
+    elif feature_format == "boolean":
+        description_text = (
+            "This graph represents a boolean feature. The keys are 'True' and 'False',"
+            " the two possible values of the feature.\n\n"
+        )
+    else:
+        raise Exception(f"Unknown feature format {feature_format}")
+
+    # simplify the graph until it fits into the max_tokens limit
+    total_tokens = None
+    min_variation_per_cent = 0.0
+    while True:
+        # simplify the graph
+        simplified_graph = graph
+        if feature_format == "continuous":
+            simplified_graph = simplify_graph(
+                graph, min_variation_per_cent=min_variation_per_cent
+            )
+
+        # confidence bounds via normal approximation
+        scores = simplified_graph.scores
+        if confidence_bounds:
+            factor = scipy.stats.norm.interval(confidence_level, loc=0, scale=1)[1]
+            lower_bounds = [
+                scores[idx] - factor * simplified_graph.stds[idx]
+                for idx in range(len(scores))
+            ]
+            upper_bounds = [
+                scores[idx] + factor * simplified_graph.stds[idx]
+                for idx in range(len(scores))
+            ]
+
+        # adjust the precision on the x-axis. this can be dangerous if we don't know the distribution of the data (it can impact the accuracy).
+        # however, this is a necessary formatting feature because certain (old?) versions of EBMs introduce trailing digits for the xbins (e.g. age as a floating point number with 10 significant digits)
+        x_vals = simplified_graph.x_vals
+        if x_axis_precision is not None:
+            x_vals = [
+                (
+                    np.round(x[0], x_axis_precision).astype(str),
+                    np.round(x[1], x_axis_precision).astype(str),
+                )
+                for x in x_vals
+            ]
+
+        # adjust the precision on the y-axis
+        if y_axis_precision == "auto":  # at least 3 significant digits
+            total_variation = np.max(scores) - np.min(scores)
+            y_fraction = total_variation / 100.0
+            y_axis_precision = 1
+            while y_fraction < 1:
+                y_fraction *= 10
+                y_axis_precision += 1
+        scores = np.round(scores, y_axis_precision).astype(str)
+        if confidence_bounds:
+            lower_bounds = np.round(lower_bounds, y_axis_precision).astype(str)
+            upper_bounds = np.round(upper_bounds, y_axis_precision).astype(str)
+
+        # formatting for boolean features
+        if feature_format == "boolean":
+            assert len(x_vals) == 2, (
+                "Requested a boolean format, but the feature has more than x-axis"
+                f" values: {x_vals}"
+            )
+            # we assume that the left key (the lower numer, typically -1 or 0), is 'False'
+            x_vals = ["False", "True"]
+
+        # create the textual representation of the graph
+        prompt = ""
+        if include_description:
+            prompt += description_text
+        prompt += f"Feature Name: {graph.feature_name}\n"
+        prompt += f"Feature Type: {feature_format}\n"
+        prompt += f"Means: {xy_to_json_(x_vals, scores)}\n"
+        if confidence_bounds:
+            prompt += (
+                f"Lower Bounds ({confidence_level*100:.0f}%-Confidence Interval):"
+                f" {xy_to_json_(x_vals, lower_bounds)}\n"
+            )
+            prompt += (
+                f"Upper Bounds ({confidence_level*100:.0f}%-Confidence Interval):"
+                f" {xy_to_json_(x_vals, upper_bounds)}\n"
+            )
+
+        # count the number of tokens
+        total_tokens = num_tokens_from_string_(prompt, "gpt-4")
+        if feature_format == "continuous":
+            if total_tokens > max_tokens:
+                if min_variation_per_cent > 0.1:
+                    raise Exception(
+                        f"The graph for feature {graph.feature_name} of type"
+                        f" {graph.feature_type} requires {total_tokens} tokens even at"
+                        " a simplification level of 10%. This graph is too complex to"
+                        " be passed to the LLM within the loken limit of"
+                        f" {max_tokens} tokens."
+                    )
+                min_variation_per_cent += 0.001
+            else:
+                if min_variation_per_cent > 0:
+                    print(
+                        f"INFO: The graph of feature {graph.feature_name} was"
+                        f" simplified by {min_variation_per_cent * 100:.1f}%."
+                    )
+                return prompt
+        else:
+            if total_tokens > max_tokens:
+                raise Exception(
+                    f"The graph for feature {graph.feature_name} of type"
+                    f" {graph.feature_type} requires {total_tokens} tokens and exceeds"
+                    f" the token limit of {max_tokens}."
+                )
+            else:
+                return prompt
